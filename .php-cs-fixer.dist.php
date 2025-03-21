19c1be7799d28df34c761dd65192b8110562e492
Patrizio Bekerle <patrizio@bekerle.com>
2025-03-21 20:35:26 +0100

    php-cs-fixer: add Nextcloud coding-standards
    Signed-off-by: Patrizio Bekerle <patrizio@bekerle.com>

--- .php-cs-fixer.dist.php
+++ .php-cs-fixer.dist.php
@@ -1,25 +1,16 @@
 <?php
 
-$finder = PhpCsFixer\Finder::create()
-    ->in(__DIR__)
-    ->exclude('var')
-;
+declare(strict_types=1);
 
-$config = new PhpCsFixer\Config();
-$config->setRules([
-    '@Symfony' => true,
-    '@PHP70Migration' => true,
-    '@PHP71Migration' => true,
-    '@PHP73Migration' => true,
-    '@DoctrineAnnotation' => true,
-    'doctrine_annotation_array_assignment' => ['operator' => '='],
-    'yoda_style' => false,
-    'strict_comparison' => true,
-    'strict_param' => true,
-    'declare_strict_types' => true,
-    'method_argument_space' => ['on_multiline' => 'ignore'],
-])
-    ->setRiskyAllowed(true)
-    ->setFinder($finder);
+require_once './vendor/autoload.php';
 
+use Nextcloud\CodingStandard\Config;
+
+$config = new Config();
+$config
+	->getFinder()
+	->ignoreVCSIgnored(true)
+	->notPath('build')
+	->notPath('vendor')
+	->in(__DIR__);
 return $config;

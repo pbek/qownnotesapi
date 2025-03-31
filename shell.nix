918d44f681a1f1654bc2493f8d32022c51acbecb
Patrizio Bekerle <patrizio@bekerle.com>
2025-03-31 21:58:19 +0200

    treefmt: add pre-commit hook
    Signed-off-by: Patrizio Bekerle <patrizio@bekerle.com>

--- shell.nix
+++ shell.nix
@@ -11,4 +11,15 @@ pkgs.mkShell {
     docker-slim
     act # Run GitHub Actions locally
   ];
+
+  shellHook = ''
+    # Determine the repository root
+    REPO_ROOT=$(git rev-parse --show-toplevel 2>/dev/null)
+
+    # Check if we are in the repository root
+    if [ "$REPO_ROOT" = "$(pwd)" ]; then
+      # Symlink the pre-commit hook into the .git/hooks directory
+      ln -sf ../../scripts/pre-commit.sh .git/hooks/pre-commit
+    fi
+  '';
 }

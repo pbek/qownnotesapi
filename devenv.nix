256c9b8e54377272046c6dc9bccad30cb911ea7c
Patrizio Bekerle <patrizio@bekerle.com>
2026-02-04 21:05:41 +0100

    devenv: update shared
    Signed-off-by: Patrizio Bekerle <patrizio@bekerle.com>

--- devenv.nix
+++ devenv.nix
@@ -1,6 +1,5 @@
 {
   pkgs,
-  config,
   ...
 }:
 
@@ -17,14 +16,6 @@
   # https://devenv.sh/git-hooks/
   git-hooks = {
     excludes = [ "appinfo/signature.json" ];
-    hooks = {
-      php-cs-fixer = {
-        # Override the entry to use vendor version instead of Nix-provided version
-        # The Nix version (3.87.2) is too old and incompatible with PHP 8.4
-        # The vendor version (3.93.1) works correctly
-        entry = "${config.languages.php.package}/bin/php vendor/bin/php-cs-fixer fix";
-      };
-    };
   };
 
   # See full reference at https://devenv.sh/reference/options/
